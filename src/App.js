5a404bf7c33ad07ee3fb512aeeb03a7d9042e20b
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-05 15:35:38 +0530

    connected the store with the app.js with help of useSelector and useDispatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import {incNumber,decNumber} from './actions/index'
+
+import { useSelector,useDispatch } from 'react-redux'
 
 const App = () => {
+  const myState = useSelector((state)=>state.changeNumber);
+  const dispatch = useDispatch();
   return (
     <div>
       <div style={{display:"flex",alignItems:"center",justifyContent:"center",flexWrap:"wrap"}}>
@@ -8,8 +13,8 @@ const App = () => {
         <div align="center" style={{width:"100vw"}}>Using React Redux</div>
         <div align="center" style={{width:"100vw"}}>
           <div>
-            <button><h1>-</h1></button>
-            count
+            <button onClick={()=>dispatch(decNumber())}><h1>-</h1></button>
+            {myState}
             <button><h1>+</h1></button>
           </div>
       </div>
