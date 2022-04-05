71d79c38a56ec2b2c2b52872d9994815e7ba1352
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-05 14:26:16 +0530

    added the ui for increment and decrement counter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div style={{display:"flex",alignItems:"center",justifyContent:"center",flexWrap:"wrap"}}>
+        <h1 align="center" style={{width:"100vw"}}>Increment and Decrement counter</h1>
+        <div align="center" style={{width:"100vw"}}>Using React Redux</div>
+        <div align="center" style={{width:"100vw"}}>
+          <div>
+            <button><h1>-</h1></button>
+            count
+            <button><h1>+</h1></button>
+          </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
