b7234393dab11e3bdd94ad87ecd98e4d70d3e26a
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-06 05:22:23 +0530

    added explanation for the actions/index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
+//  here we are defining two functions named incNumber and decNumber
+// both of these functions return simple strings particularly
+// "INCREMENT" and "DECREMENT" which will be useful for dispatching actions 
+// while using usedispatch in the component where the redeux store will be used..
+
+
+
 export const incNumber = () =>{
     return {
         type:"INCREMENT"
