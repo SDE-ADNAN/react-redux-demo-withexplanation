9cae6504441bbfbcee5844a8c4b658884f6a2394
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-05 15:21:51 +0530

    provided the store to the App component in the index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//importing the store first
+import store from './store';
+
+//importing the provider from react-redux
+import {Provider} from 'react-redux'
+
+store.subscribe(()=>{
+      console.log(store.getState);
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
