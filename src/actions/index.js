f7eccffa70a3323ea442fa8abcad77746a4493b9
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-06 06:06:52 +0530

    added explanation for the actions/index.js file with some more modifs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
+// this is the first step in using redux (defining action )
+
+
 //  here we are defining two functions named incNumber and decNumber
 // both of these functions return simple strings particularly
-// "INCREMENT" and "DECREMENT" which will be useful for dispatching actions 
+// "INCREMENT" and "DECREMENT" which will be useful for dispatching actions
 // while using usedispatch in the component where the redeux store will be used..
 
 
